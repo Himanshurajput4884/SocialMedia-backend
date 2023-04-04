f278f33324021d774e0fac1827a2f79370cabe1d
Himanshu <himanshurajput6654@gmail.com>
2023-04-04 18:29:00 +0530

    Adding Cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookiParser());
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://sm-frontend-rouge.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
