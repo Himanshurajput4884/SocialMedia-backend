48cbe751346b41bc4305cd00cc665b3f12cea532
Himanshu <himanshurajput6654@gmail.com>
2023-04-04 17:55:05 +0530

    Adding strictQuery to database

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DB,{
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    strictQuery: true,
 }).then(()=> console.log("DataBase Connected")).catch((errr)=>{
     console.log(errr);
 })
