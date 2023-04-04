554e8b501067384ef1846ced83fa76adeea7f7dd
Himanshu <himanshurajput6654@gmail.com>
2023-04-04 19:26:27 +0530

    Adding URL

--- db/conn.js
+++ db/conn.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.DB,{
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    strictQuery: true,
 }).then(()=> console.log("DataBase Connected")).catch((errr)=>{
     console.log(errr);
 })
